120e8377dc654fae423ac5a84586cd214c3bfdc6
MikelEngel <angelhevel@gmail.com>
2022-12-06 16:55:27 -0600

    productos add

--- ecommerce-app/src/App.js
+++ ecommerce-app/src/App.js
@@ -9,7 +9,7 @@ function App() {
       </header>
       <h1>Nuestros productos</h1>
       {data.products.map((product) => (
-        <div>
+        <div key={product.slug}>
           <img src={product.image} alt={product.name} />
           <p>{product.name}</p>
           <p>{product.price}</p>
