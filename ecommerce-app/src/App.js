2902dc78779e0ae8e489a453dfb36495fd4c2f0f
MikelEngel <angelhevel@gmail.com>
2022-12-06 16:50:46 -0600

    productos creados

--- ecommerce-app/src/App.js
+++ ecommerce-app/src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import data from './data';
 
 function App() {
   return (
     <div>
       <header>
-        <a href="/">E-Commerce</a>
+        <a href="/">Aoking E-Commerce</a>
       </header>
+      <h1>Nuestros productos</h1>
+      {data.products.map((product) => (
+        <div>
+          <img src={product.image} alt={product.name} />
+          <p>{product.name}</p>
+          <p>{product.price}</p>
+        </div>
+      ))}
     </div>
   );
 }
